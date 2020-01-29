3022dd43caac8632dbc22a7a7fbbbc529f023ecb
William Stephens <william.a.stephens17@gmail.com>
2020-01-29 10:52:09 -0600

    StringExercise

---
+++ src/StringExercise.java
@@ -0,0 +1,14 @@
+public class StringExercise {
+    public static void main(String[] args) {
+//        String output = "We don't need no education";
+//        System.out.println(output);
+//        System.out.println(output.replace("education", "thought control"));
+//        String outputTwo = "Check \"this\" out!, \"s inside of \"s!";
+//        System.out.println(outputTwo);
+//        String outputThree = "In windows, the main drive is usually C:\\\\,\n" +
+//                "and the amazing triple backslash \\\\\\!";
+//        System.out.println(outputThree);
+
+
+    }
+}
--- src/StringLecture.java
+++ src/StringLecture.java
@@ -1,10 +1,23 @@
 public class StringLecture {
     public static void main(String[] args) {
-        String message = "Hello";
-        String s = "Hello"; //Hello and s are currently pointing to the same place in memory
-        System.out.println(message);
-        message = message + " World";
-        System.out.println(message);
+
+//        String message = "Hello";
+//        String s = "Hello"; //Hello and s are currently pointing to the same place in memory
+//        System.out.println(message);
+//        message = message + " World";
+//        System.out.println(message);
+
+//        String compare = "Hello Codeup";
+//        String otherCompare = "hello Codeup";
+//        boolean equals = compare.equals(otherCompare);
+//        System.out.println(equals); //False
+//        boolean otherEquals = compare.equalsIgnoreCase(otherCompare);
+//        System.out.println(otherEquals);
+
+        String hello = "Hi";
+        System.out.println("Hi".startsWith("H"));
+
+
     }
 
     public static long sum(int inputNumber) {
