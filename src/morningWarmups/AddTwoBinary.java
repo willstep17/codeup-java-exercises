9a3188cc7ea4d3575324fade9c9a43693c348408
William Stephens <william.a.stephens17@gmail.com>
2020-02-03 09:28:21 -0600

    Addtwo binary

--- src/morningWarmups/AddTwoBinary.java
+++ src/morningWarmups/AddTwoBinary.java
@@ -3,11 +3,16 @@ package morningWarmups;
 public class AddTwoBinary {
     public static void main(String[] args) {
 
-        System.out.println(addTwoBinary(3, 5));
+        int[] binaryArray = addTwoBinary(2, 5);
+        for(int i=0;i<binaryArray.length;i++) {
+            System.out.println(binaryArray[i]);
+        }
+
+//        System.out.println(binaryMath("1010", "1111"));
 
     }
 
-    public static int[] addTwoBinary(long inputOne,long inputTwo) {
+    public static int[] addTwoBinary (long inputOne,long inputTwo) {
         int i = 0, carry = 0;
         int[] sum = new int[10];
         while (inputOne != 0 || inputTwo != 0) {
