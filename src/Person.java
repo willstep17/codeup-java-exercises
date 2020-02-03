738c8250acb72257f4ad1157acc81d98702f7111
William Stephens <william.a.stephens17@gmail.com>
2020-02-03 16:37:08 -0600

    Polishing classes

--- src/Person.java
+++ src/Person.java
@@ -2,10 +2,6 @@ public class Person {
     private String name;
     private int age;
 
-//    public Person(String inputName) {
-//        this.name = inputName;
-//    }
-
     public Person(String inputName, int inputAge) {
         this.name = inputName;
         this.age = inputAge;
@@ -15,37 +11,7 @@ public class Person {
         this.name = inputName;
     }
 
-    public Person() {
-
-    }
-
-    public static void main(String[] args) {
-        Person chris = new Person("Chris", 32);
-        Person will = new Person("Will");
-        Person bill = new Person();
-        will.name = "William";
-
-//        System.out.println("Return from will.getName(): " + will.getName());
-//        will.setName("William");
-//        will.sayHello();
-
-//        Person person1 = new Person("John");
-//        Person person2 = new Person("John");
-//        System.out.println(person1.getName().equals(person2.getName()));
-//        System.out.println(person1 == person2);
-
-//        Person person1 = new Person("John");
-//        Person person2 = person1;
-//        System.out.println(person1 == person2);
-
-//        Person person1 = new Person("John");
-//        Person person2 = person1;
-//        System.out.println(person1.getName());
-//        System.out.println(person2.getName());
-//        person2.setName("Jane");
-//        System.out.println(person1.getName());
-//        System.out.println(person2.getName());
-    }
+    public Person() { }
 
     public String getName() {
         return this.name;
@@ -54,6 +20,10 @@ public class Person {
     public void setName(String inputName) {
         this.name = inputName;
     }
+
+    public int getAge() { return this.age; }
+
+    public void setAge(int inputAge) { this.age = inputAge; }
 
     public void sayHello() {
         System.out.println(this.name + " says hello!");
--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -11,14 +11,16 @@ public class MoviesApplication {
         Movie[] movieArray = MoviesArray.findAll();
 
         while(true) {
-            System.out.println("What would you like to do?\n");
+            System.out.println("\nWhat would you like to do?");
             System.out.println("0 - Exit");
             System.out.println("1 - Add a Movie");
             System.out.println("2 - View All Movies");
             System.out.println("3 - View Movies in the Animated Category");
             System.out.println("4 - View Movies in the Drama Category");
             System.out.println("5 - View Movies in the Horror Category");
-            System.out.println("6 - View Movies in the SciFi Category\n");
+            System.out.println("6 - View Movies in the SciFi Category");
+            System.out.println("7 - View Movies in the Musical Category");
+            System.out.println("8 - View Movies in the Comedy Category");
             switch(input.getInt("Enter Your Choice: ")) {
                 case 0:
                     return;
@@ -44,6 +46,14 @@ public class MoviesApplication {
                 case 6:
                     printMovies(movieArray, "scifi");
                     break;
+                case 7:
+                    printMovies(movieArray, "musical");
+                    break;
+                case 8:
+                    printMovies(movieArray, "comedy");
+                    break;
+                default:
+                    break;
             }
         }
     }
@@ -51,14 +61,14 @@ public class MoviesApplication {
     public static void printMovies(Movie[] inputMovies, String inputCategory) {
         for(Movie movie : inputMovies) {
             if(movie.getCategory().equals(inputCategory)) {
-                System.out.println(movie.getName() + " -- " + movie.getCategory());
+                System.out.println(movie.getName() + " -- " + capitalize(movie.getCategory()));
             }
         }
     }
 
     public static void printMovies(Movie[] inputMovies) {
         for(Movie movie : inputMovies) {
-                System.out.println(movie.getName() + " -- " + movie.getCategory());
+                System.out.println(movie.getName() + " -- " + capitalize(movie.getCategory()));
         }
     }
 
@@ -67,6 +77,11 @@ public class MoviesApplication {
         Movie newMovie = new Movie(newMovieName, newMovieCategory);
         returnMovies[inputMovies.length] = newMovie;
         return returnMovies;
+    }
+
+    public static String capitalize(String inputString) {
+        String firstLetter = inputString.substring(0, 1).toUpperCase();
+        return firstLetter + inputString.substring(1);
     }
 
 }
