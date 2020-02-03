dfb71c948ef22529ae0c9edef96acbe4bdc43892
William Stephens <william.a.stephens17@gmail.com>
2020-02-03 11:06:24 -0600

    ServerNameGenerator

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class ServerNameGenerator {
+    public static String[] adjectives = {"Superficial", "Separate", "Industrious", "Astonishing", "Eastern", "Cooing", "Terrific", "Unaccountable", "Optimal", "Jolly", "Funny", "Envious", "Habitual"};
+    public static String[] nouns = {"Resolution", "Potato", "Chalkboard", "Studio", "Wealth", "Energy", "Homework", "Army", "Passenger", "Penalty", "Variety", "Road", "Contract"};
+
+    public static void main(String[] args) {
+
+        for(int i=0;i<20;i++) {
+            System.out.println(returnRandom(adjectives) + "-" + returnRandom(nouns));
+        }
+
+    }
+
+    public static String returnRandom(String[] inputArr) {
+        Random random = new Random();
+        return inputArr[random.nextInt(inputArr.length - 1)];
+    }
+
+}
