0080f4c190a4c2c6c2f8ab48c4874aba8930a631
William Stephens <william.a.stephens17@gmail.com>
2020-02-04 11:02:53 -0600

    Inheritance lecture

--- src/inheritance/InheritanceTest.java
+++ src/inheritance/InheritanceTest.java
@@ -2,21 +2,29 @@ package inheritance;
 
 public class InheritanceTest {
     public static void main(String[] args) {
+        Vehicle[] garage = new Vehicle[4];
+
         Vehicle vehicleOne = new Vehicle(5);
         vehicleOne.numberOfTires = 1;
         vehicleOne.go();
         vehicleOne.report();
+        garage[0] = vehicleOne; //Fits in Vehicle garage[] because it is a vehicle
 
         Car carOne = new Car();
         carOne.go();
         carOne.report();
+        garage[1] = carOne; //Knows it is a vehicle. Polymorphism.
 
         Truck truckOne = new Truck();
         truckOne.go();
         truckOne.report();
+        garage[2] = truckOne;
 
         Motorcycle motoOne = new Motorcycle();
         motoOne.go();
         motoOne.report();
+        garage[3] = motoOne;
+
+
     }
 }
