605111f9bea8dc00e129ef85308dba594b949d20
William Stephens <william.a.stephens17@gmail.com>
2020-02-07 09:24:29 -0600

    Update morning warmup

--- src/morningWarmups/StringIteration.java
+++ src/morningWarmups/StringIteration.java
@@ -1,4 +1,35 @@
 package morningWarmups;
 
-public class StringItteration {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringIteration {
+    public static void main(String[] args) {
+        String[] stringArray = {"First String", "Dinosaur", "Dracula is a vampire.", "Fourth String", "Do you believe in love after life"};
+        ArrayList<String> newArrayList = new ArrayList<>();
+
+        for(String str : stringArray) {
+            newArrayList.add(str);
+        }
+        for(String ALstr : newArrayList) {
+            System.out.println(ALstr);
+        }
+
+        HashMap<Integer, String> newHashMap = new HashMap<>();
+        for(int i=0;i<5;i++) {
+            newHashMap.put(i, newArrayList.get(i));
+        }
+
+        for(int i=0;i<newHashMap.size();i++) {
+            System.out.println(newHashMap.get(i));
+        }
+
+        String[] toBePutIn = new String[newArrayList.size()];
+        for(int i=0;i<newArrayList.size();i++) {
+            toBePutIn[i] = newArrayList.get(i);
+            System.out.println(toBePutIn[i]);
+        }
+
+    }
 }
