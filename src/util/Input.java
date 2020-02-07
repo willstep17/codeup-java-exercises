19c8643e3adfef59b440bb7c8ccbc3e7e37d5bd0
William Stephens <william.a.stephens17@gmail.com>
2020-02-07 12:39:47 -0600

    Updating getInt and getDouble

--- src/util/Input.java
+++ src/util/Input.java
@@ -31,41 +31,90 @@ public class Input {
         return userInput.toLowerCase().startsWith("y");
     }
 
+    public int getInt() {
+        String userInput = "";
+        boolean invalid = true;
+        int returnInt = 0;
+        while(invalid) {
+            try {
+                System.out.print("Enter an integer: ");
+                userInput = this.scanner.nextLine();
+                returnInt = Integer.parseInt(userInput);
+                invalid = false;
+            } catch (NumberFormatException e) {
+                System.out.print(userInput + " is not a valid integer. ");
+            }
+        }
+        return returnInt;
+    }
+
+    public int getInt(String prompt) {
+        String userInput = "";
+        boolean invalid = true;
+        int returnInt = 0;
+        while(invalid) {
+            try {
+                System.out.print(prompt);
+                userInput = this.scanner.nextLine();
+                returnInt = Integer.parseInt(userInput);
+                invalid = false;
+            } catch (NumberFormatException e) {
+                System.out.print(userInput + " is not a valid integer. ");
+            }
+        }
+        return returnInt;
+    }
+
     public int getInt(int min, int max) {
-        int userInput;
+        int userInput = 0;
         do {
             System.out.print("Enter an int between " + min + " and " + max + ": ");
-            userInput = this.scanner.nextInt();
+            userInput = getInt();
         } while (min >= userInput && userInput <= max);
         return userInput;
     }
 
-    public int getInt() {
-        System.out.print("Enter an integer: ");
-        return this.scanner.nextInt();
+    public double getDouble() {
+        String userInput = "";
+        boolean invalid = true;
+        Double returnDouble = 0.0;
+        while (invalid) {
+            try {
+                System.out.print("Enter a double: ");
+                userInput = this.scanner.nextLine();
+                returnDouble = Double.parseDouble(userInput);
+                invalid = false;
+            } catch (NumberFormatException e) {
+                System.out.println(userInput + " is not a valid double. ");
+            }
+        }
+        return returnDouble;
     }
 
-    public int getInt(String prompt) {
-        System.out.print(prompt);
-        return this.scanner.nextInt();
+    public double getDouble(String prompt) {
+        String userInput = "";
+        boolean invalid = true;
+        Double returnDouble = 0.0;
+        while (invalid) {
+            try {
+                System.out.print(prompt);
+                userInput = this.scanner.nextLine();
+                returnDouble = Double.parseDouble(userInput);
+                invalid = false;
+            } catch (NumberFormatException e) {
+                System.out.println(userInput + " is not a valid double. ");
+            }
+        }
+        return returnDouble;
     }
 
     public double getDouble(double min, double max) {
         double userInput;
         do {
             System.out.print("Enter a double between " + min + " and " + max + ": ");
-            userInput = this.scanner.nextDouble();
+            userInput = getDouble();
         } while (min >= userInput && userInput <= max);
         return userInput;
     }
 
-    public double getDouble() {
-        System.out.print("Enter a double: ");
-        return this.scanner.nextDouble();
-    }
-
-    public double getDouble(String prompt) {
-        System.out.print(prompt);
-        return this.scanner.nextDouble();
-    }
 }
