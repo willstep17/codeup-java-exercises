9fa2ee3434fdf4ce825281c18e3030774f93bb00
William Stephens <william.a.stephens17@gmail.com>
2020-02-11 10:33:16 -0600

    Morning warmup

--- src/Playground.java
+++ src/Playground.java
@@ -1,18 +1,7 @@
-import shapes.Circle;
-import util.Input;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Playground {
     public static void main(String[] args) {
-//        String test = "Start";
-//        test += 21;
-//        System.out.println(test);
-
-//        Person newPerson = new Person("Jim", 43);
-//        newPerson.getName()
-
-        Input input = new Input();
-        Circle circle = new Circle(input.getDouble("Enter a radius: "));
-        System.out.println(circle.getArea());
 
     }
 }
