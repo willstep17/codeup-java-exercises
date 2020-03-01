09dfaa6622d648c6a6eacb1efda46ec3e37047d6
William Stephens <william.a.stephens17@gmail.com>
2020-02-29 19:17:01 -0600

    Collections Practice

--- src/morningWarmups/productOf.java
+++ src/morningWarmups/productOf.java
@@ -9,20 +9,21 @@ public class productOf {
         testArr.add(2);
         testArr.add(3);
         ArrayList<Integer> returnArr = productOf(testArr);
-        for(int i=0;i<3;i++) {
+        for (int i = 0; i < 3; i++) {
             System.out.println(returnArr.get(i));
         }
     }
 
     public static ArrayList<Integer> productOf(ArrayList<Integer> inputArr) {
         int totalMultiplied = 1;
-        for(int i=0;i<inputArr.size();i++) {
+        for (int i = 0; i < inputArr.size(); i++) {
             totalMultiplied *= inputArr.get(i);
         }
         ArrayList<Integer> returnArr = new ArrayList<>();
-        for(int j=0;j<inputArr.size();j++) {
+        for (int j = 0; j < inputArr.size(); j++) {
             returnArr.add(totalMultiplied / inputArr.get(j));
         }
         return returnArr;
     }
+}
 
