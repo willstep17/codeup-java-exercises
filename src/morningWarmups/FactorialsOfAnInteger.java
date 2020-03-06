f26e55d385570a6b7295fbf0359706c5e0e09aa3
William Stephens <william.a.stephens17@gmail.com>
2020-03-06 10:37:12 -0600

    morning warmup

--- src/morningWarmups/FactorialsOfAnInteger.java
+++ src/morningWarmups/FactorialsOfAnInteger.java
@@ -4,11 +4,20 @@ import java.util.ArrayList;
 
 public class FactorialsOfAnInteger {
     public static void main(String[] args) {
+        ArrayList<Integer> testList = factorialOfInteger(6);
+        for(int i : testList) {
+            System.out.println(i);
+        }
 
     }
 
     public static ArrayList<Integer> factorialOfInteger(int inputInt) {
         ArrayList<Integer> factors = new ArrayList<>();
-        for
+        for(int i=1;i<=inputInt;i++) {
+            if(inputInt % i == 0) {
+                factors.add(i);
+            }
+        }
+        return factors;
     }
 }
