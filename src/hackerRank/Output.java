39970b5e4694d7693cafa6ef3307691109d96fe7
William Stephens <william.a.stephens17@gmail.com>
2020-06-06 21:54:53 -0500

    .

--- src/hackerRank/Output.java
+++ src/hackerRank/Output.java
@@ -19,7 +19,7 @@ public class Output {
         System.out.println("================================");
         for(int i=0;i<3;i++)
         {
-            String s1=sc.next();
+            String s1=sc.next()
             int x=sc.nextInt();
             System.out.printf("%-15s%03d%n", s1, x);
         }
